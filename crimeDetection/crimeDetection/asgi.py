daef4451b06c1255256e84eee8446a8809249a54
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-04-28 05:46:02 +0300

    Create CrimeDetection

--- crimeDetection/crimeDetection/asgi.py
+++ crimeDetection/crimeDetection/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for crimeDetection project.
+ASGI config for CrimeDetection project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crimeDetection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrimeDetection.settings')
 
 application = get_asgi_application()
