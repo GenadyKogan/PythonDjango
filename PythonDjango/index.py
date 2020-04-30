ef77a530cfc194a429951c3da7e9dd0e35a3bbdc
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-04-30 18:19:17 +0300

    test

---
+++ PythonDjango/index.py
@@ -0,0 +1,25 @@
+import tornado.web
+import tornado.ioloop
+
+
+
+class uploadImgHandler(tornado.web.RequestHandler):
+    def post(self):
+        files = self.request.files["fileImage"]
+        for f in files:
+            fh = open(f"img/{f.filename}", "wb")
+            fh.write(f.body)
+            fh.close()
+        self.write(f"http://localhost:1024/img/{f.filename}")
+    def get(self):
+        self.render("index.html")
+
+if (__name__ == "__main__"):
+    app = tornado.web.Application([
+        ("/", uploadImgHandler),
+        ("/img/(.*)", tornado.web.StaticFileHandler, {'path': 'img'})
+    ])
+
+    app.listen(1024)
+    print("Listening on port 8080")
+    tornado.ioloop.IOLoop.instance().start()
