295fbb07d531944fd9f91673ed979ff9199ed76a
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-05-04 14:40:22 +0300

    comtact information

--- CrimeDetection/mainApp/views.py
+++ CrimeDetection/mainApp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainApp/homePage.html')
 def contact(request):
-    return render(request, 'mainApp/basic.html', {'values': ['If you have any questions:', '086424242']})
+    return render(request, 'mainApp/basic.html', {'values': ['If you have any questions:', 'rgkogan@gmail.com']})
 
 
