d2efb4ab6ea81518d8e07d0a278792b3188d0277
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-06-11 17:56:01 +0300

    test

--- CrimeDetection/CrimeDetection/mainApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'mainApp/homePage.html')
-def contact(request):
-    return render(request, 'mainApp/basic.html', {'values': ['If you have any questions:', 'rgkogan@gmail.com']})
-
-
--- CrimeDetection/CrimeDetection/uploadApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-  url('', views.upload,name='upload'),
-]
